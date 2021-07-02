6b000f3241127ad7edf4d53a649fd52b86ae2357
Nikola <nikola.krstic99@hotmail.com>
2021-07-02 20:04:03 +0200

    zavrseno dodavanje recepata

---
+++ Implementation/skripta.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    
+    $("#dodajReceptDugme").click(function(){
+        
+        let ime_recepta=document.getElementById("ime_recepta").value;
+        let vrsta_jela=document.getElementById("vrsta_jela").value;
+        let recept=document.getElementById("recept").value;
+        let tezina=document.getElementById("tezina").value;
+        let vreme=document.getElementById("vreme").value;
+        let slika=document.getElementById("slika").value;
+        let recepti=[];
+        if(localStorage.getItem("recepti")!=null){
+            recepti=JSON.parse(localStorage.getItem("recepti"));
+        }
+        recepti.push(
+            {
+                ime_recepta:ime_recepta,
+                vrsta_jela:vrsta_jela,
+                recept:recept,
+                tezina:tezina,
+                vreme:vreme,
+                slika:slika
+            }
+        );
+        localStorage.setItem("recepti",JSON.stringify(recepti));
+    });
+});
